add5fbf357d796cc4a735d6cb6a39a5f85422e6b
fhoekstra <32362869+fhoekstra@users.noreply.github.com>
2026-01-29 22:19:35 +0100

    Adapt config to Rockchip cluster (#466)
    * feat: Add initial booting rockchip Rock 5B Talos configuration
    
    Using an SD card as read-only root fs, and the NVMe for everything that
    gets written. PoC with one node and starting Talos, no workloads yet
    
    * Add config for other rockchip nodes, put a `u-ceph` labeled partition on each
    
    * chore: connect Cephcluster to `u-ceph` partition on each node
    
    * fix(OS): Make custom Talos ISO with unreleased but merged `rock5b-plus` overlay
    
    The regular rock5b only has 1 M.2 slot, and though you can boot Talos on
    a rock 5b plus with it, you cannot use the second M.2 slot that the plus
    has
    
    * fix(OS): Fix Talos factory schematic id, clean up Disk image script to bootstrap directory
    
    * feat(rook-ceph): Explicitly set devices per node for rook-ceph
    
    ---------
    
    Co-authored-by: Freek <dev@freekhoekstra.nl>

---
+++ kubernetes/bootstrap/talos/create-talos-disk-image-with-custom-overlay.sh
@@ -0,0 +1,15 @@
+# Sourced from: https://docs.siderolabs.com/talos/v1.11/build-and-extend-talos/custom-images-and-development/overlays#developing-an-overlay
+# Outputs metal-arm64.raw into ./_out
+# Write to SD card with:
+# sudo dd if=_out/metal-arm64.raw of=/dev/sdb conv=fsync oflag=direct status=progress bs=4M
+
+export TALOS_VERSION=v1.11.6
+export OVERLAY_IMAGE="ghcr.io/siderolabs/sbc-rockchip:v0.1.7-3-g76838fe"
+export OVERLAY_NAME="rock5b-plus"
+
+sudo docker run --rm -t -v ./_out:/out -v /dev:/dev --privileged \
+    ghcr.io/siderolabs/imager:${TALOS_VERSION} \
+    metal --arch arm64 \
+    --overlay-name="${OVERLAY_NAME}" \
+    --overlay-image="${OVERLAY_IMAGE}"
+
